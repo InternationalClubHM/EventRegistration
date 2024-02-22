27bbe6a9b5f623f3de10abf854dae576a6bc4b76
IsaSmt <isa.simmet@gmx.de>
2024-02-22 20:52:52 +0100

    added list of events

--- registration/src/app/events/events.component.ts
+++ registration/src/app/events/events.component.ts
@@ -1,10 +1,28 @@
+// events.component.ts
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss']
+  styleUrls: ['./events.component.css']
 })
 export class EventsComponent {
-
+  eventNames: string[] = ['Orientation Days',
+                          'Bavarian Brunch',
+                          'Stadttour/CityRallye',
+                          'Residenz',
+                          'Schloss Neuschwanstein',
+                          'Lindau-Insel Bodensee Schifffahrt',
+                          'Tour Olympia-Dächer',
+                          'Tour Olympia-Dächer',
+                          'Sailing',
+                          'Zugspitze',
+                          'BERLIN',
+                          'Wildwasserrafting',
+                          'KZ Dachau',
+                          'Wandern (Partnachklamm)',
+                          'Sommerrodeln',
+                          'Klettergarten',
+                          'Abschlussevent Standup-Paddeling mit Grillen an einem See'];
 }
