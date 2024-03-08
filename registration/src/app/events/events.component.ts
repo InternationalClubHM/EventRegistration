c43fcea0522d6a3e236c73a5a1301bec2fd071f9
IsaSmt <isa.simmet@gmx.de>
2024-03-08 18:23:59 +0100

    made txt file to make the events easily editable for non-github and non-IDE users

--- registration/src/app/events/events.component.ts
+++ registration/src/app/events/events.component.ts
@@ -1,64 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css']
 })
-export class EventsComponent {
-  eventNames: string[] = [
-    'Bavarian Brunch',
-    'City Rally',
-    'Residenz',
-    'Schloss Neuschwanstein',
-    'Bodensee Boat Trip',
-    'Tour Olympic Roofs',
-    'Tour Olympic Roofs',
-    'Sailing',
-    'Zugspitze',
-    'Rafting',
-    'Dachau Memorial',
-    'Hiking (Partnachklamm)',
-    'Summer Sledding',
-    'High Ropes Course',
-    'Last Event: Standup-Paddeling at a lake with BBQ'
-  ];
+export class EventsComponent implements OnInit {
+  eventNames: string[] = [];
+  eventDates: string[] = [];
+  eventLinks: string[] = [];
 
-  eventDates: string[] = [
-    'Saturday, 9th of March 2024',
-    'Saturday, 16th of March 2024',
-    'Saturday, 30th of March 2024',
-    'Saturday, 13th of April 2024',
-    'Saturday, 20th of April 2024',
-    'Saturday, 27th of April 2024',
-    'Sunday, 28th of April 2024',
-    'Saturday 4th of May 2024',
-    'Saturday 11th of May 2024',
-    'Saturday 18th of May 2024',
-    'Saturday, 25th of May 2024',
-    'Saturday 8th of June 2024',
-    'Saturday 15th of June 2024',
-    'Saturday 22nd of June 2024',
-    'Saturday 29th of June 2024'
-  ];
+  constructor(private http: HttpClient) {}
 
-  eventLinks: string[] = [
-    'https://forms.gle/NSfaSj9MCsJSHNXB7',
-    'https://docs.google.com/forms/d/e/1FAIpQLScC0SGKfuE1lh4bVllzTJRv3UFMtJ83iJVcrOxynIKgd0h8lQ/viewform?usp=sf_link',
-    'link-zu-event-1',
-    'link-zu-event-1',
-    'link-zu-event-3',
-    'link-zu-event-1',
-    'link-zu-event-2',
-    'link-zu-event-1',
-    'link-zu-event-2',
-    'link-zu-event-3',
-    'https://forms.gle/gbhBg3HeXsUrvUWz9',
-    'link-zu-event-3',
-    'link-zu-event-1',
-    'link-zu-event-2',
-    'link-zu-event-3'
-  ];
+ngOnInit(): void {
+  this.http.get('assets/events.txt', { responseType: 'text' })
+    .subscribe(data => {
+      const lines = data.split('\n');
+      lines.forEach(line => {
+        if (line.trim() !== '') {
+          const [eventName, eventDate, eventLink] = line.split(';');
+          this.eventNames.push(eventName);
+          this.eventDates.push(eventDate);
+          this.eventLinks.push(eventLink);
+        }
+      });
+    });
+}
 
   renderEventName(eventName: string, eventLink: string): string {
     if (eventLink.startsWith('link-zu-event')) {
